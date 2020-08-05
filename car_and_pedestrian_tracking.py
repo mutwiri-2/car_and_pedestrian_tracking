bab304ce77a1cbdb1466b370a7f4f811b982c594
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 11:03:07 +0300

    convert image to gray for faster processing

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -12,6 +12,9 @@ img = cv2.imread(img_file)
 # create a car classifier object
 car_tracker = cv2.CascadeClassifier(car_detection_file)
 
+# convert image to black and white - makes algorith faster
+black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 
 # display image
 cv2.imshow('Cars on a highway', img)
