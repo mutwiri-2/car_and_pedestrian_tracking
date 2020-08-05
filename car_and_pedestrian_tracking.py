b9b325a59bf27bcf0e0afa8faa4692a0bbfd220a
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 20:57:24 +0300

    randomly choose a video file

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -1,10 +1,10 @@
 """detect and track pedestrians and cars in a video"""
 
 import cv2
+import random
 
 #videos
-video_file = 'videos/dash_cam.mp4'
-# video_file = 'videos/pedestrians.mp4'
+video_file = random.choice(['videos/dash_cam.mp4', 'videos/pedestrians.mp4'])
 
 # get video using opencv
 video = cv2.VideoCapture(video_file)
