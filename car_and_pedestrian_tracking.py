b3c9d1314856598047397b15c6d0a0c5950ec80d
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 12:28:09 +0300

    create classifier objects for cars and pedestrians

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -13,6 +13,10 @@ video = cv2.VideoCapture(video_file)
 pedestrian_classifier_file = 'classifier_files/haarcascade_fullbody.xml'
 car_classifier_file = 'classifier_files/vehicle_detection_haarcascades.xml'
 
+# create classifier objects for pedestrians and cars
+car_tracker = cv2.CascadeClassifier(car_classifier_file)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_classifier_file)
+
 while True:
     # read the current frame from VideoCapture object
     read_successful, frame = video.read()
@@ -22,6 +26,8 @@ while True:
         grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     else:
         break
+
+
 
     # Display
     cv2.imshow('Cars and Pedestrians', grayscaled_frame)
