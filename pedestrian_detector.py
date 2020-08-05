809b8b6dc4793fa67c454151f5cfe3f5baab2143
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 21:43:02 +0300

    wait for 10000ms then close

--- pedestrian_detector.py
+++ pedestrian_detector.py
@@ -22,11 +22,14 @@ black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # detect pedestrians
 pedestrians = full_body_detector.detectMultiScale(black_n_white)
-print(pedestrians)
+# print(pedestrians)
 
+# draw rectangles around pedestrians
+for x, y, w, h in pedestrians:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0,100,100), 2)
 
 # display
 cv2.imshow('Pedestrians', img)
-cv2.waitKey()
+cv2.waitKey(10000)
 
 print("Code completed")
