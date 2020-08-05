3fc53378e3d931a55aa6dad36ef62418b78b7114
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 21:18:13 +0300

    read and show image using opencv

--- pedestrian_detector.py
+++ pedestrian_detector.py
@@ -8,4 +8,12 @@ img_file = random.choice(
     ['images/pedestrians.jpg', 'images/pedestrians2.jpg', 'images/pedestrians3.jpg']
 )
 
+# read image in opencv format
+img = cv2.imread(img_file)
+
+
+# display
+cv2.imshow('Pedestrians' ,img)
+cv2.waitKey()
+
 print("Code completed")
