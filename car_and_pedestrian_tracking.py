ea563ab8a968c29d616f2d680b680919a5864b58
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 11:54:33 +0300

    load car and pedestrian classifier files

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -6,5 +6,9 @@ import cv2
 video_file = 'videos/dash_cam.mp4'
 video_file = 'videos/pedestrians.mp4'
 
-
+# read video using opencv
 video = cv2.VideoCapture(video_file)
+
+# classifier files
+pedestrian_classifier_file = 'classifier_files/haarcascade_fullbody.xml'
+car_classifier_file = 'classifier_files/vehicle_detection_haarcascades.xml'
