61b74b1b4ff0df0c4ee0e86cdf21e2303201954f
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 21:14:12 +0300

    add pedestrian image files

---
+++ pedestrian_detector.py
@@ -0,0 +1,11 @@
+"""detect pedestrians in an image"""
+
+import cv2
+import random
+
+# image files
+img_file = random.choice(
+    ['images/pedestrians.jpg', 'images/pedestrians2.jpg', 'images/pedestrians3.jpg']
+)
+
+print("Code completed")
