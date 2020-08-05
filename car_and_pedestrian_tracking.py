3d5a2f9b803c32d83d00ee5438548917f4e4db74
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 10:54:18 +0300

    read and display image using opencv

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -6,4 +6,15 @@ img_file = 'images/cars.jpg'
 # pre-trained car classifier
 car_detection_file = 'classifier_files/vehicle_detection_haarcascades.xml'
 
+# create opencv image
+img = cv2.imread(img_file)
+
+
+# display image
+cv2.imshow('Cars on a highway', img)
+
+
+# Wait for key press - Don't autoclose
+cv2.waitKey()
+
 print("Code completed")
