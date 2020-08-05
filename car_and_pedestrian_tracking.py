e47dff0e79ed870b0b0a890f115ebfac2d838f69
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 11:49:48 +0300

    add video files

---
+++ car_and_pedestrian_tracking.py
@@ -0,0 +1,10 @@
+"""detect and track pedestrians and cars in a video"""
+
+import cv2
+
+#videos
+video_file = 'videos/dash_cam.mp4'
+video_file = 'videos/pedestrians.mp4'
+
+
+video = cv2.VideoCapture(video_file)
