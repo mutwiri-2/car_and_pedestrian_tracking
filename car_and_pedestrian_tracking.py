63e99070d995e29ee906728ddca4dc8cb8676225
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 10:43:39 +0300

    add pre-trained classifier xml files

--- car_and_pedestrian_tracking.py
+++ car_and_pedestrian_tracking.py
@@ -3,4 +3,7 @@ import cv2
 # image file
 img_file = 'images/cars.jpg'
 
+# pre-trained car classifier
+car_detection_file = 'classifier_files/vehicle_detection_haarcascades.xml'
+
 print("Code completed")
