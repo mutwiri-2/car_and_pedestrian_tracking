7deed773a081a337db5b6a4c46e18090bccb7643
mutwiri-2 <devmutwiri2@gmail.com>
2020-08-05 11:31:11 +0300

    use different images

--- car_detector.py
+++ car_detector.py
@@ -4,6 +4,8 @@ import cv2
 
 # image file
 img_file = 'images/cars.jpg'
+img_file = 'images/cars2.jpg'
+img_file = 'images/cars3.jpg'
 
 # pre-trained car classifier
 car_detection_file = 'classifier_files/vehicle_detection_haarcascades.xml'
